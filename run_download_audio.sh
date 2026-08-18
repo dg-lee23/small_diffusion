be3b324103e98d51f6aaf84bd6a92fca954f0f87
Donggyu Lee <47566711+dg-lee23@users.noreply.github.com>
2026-08-18 14:01:58 +0000

    add 2 categories to plan.md

--- run_download_audio.sh
+++ run_download_audio.sh
@@ -5,7 +5,7 @@
 #   --dry-run --limit 5
 
 python download_tracks.py \
-  --in terraria_cave_underground.csv \
+  --in tracks_terraria.csv \
   --out-dir audio/cave_underground \
   --metadata-out metadata_terraria.csv \
   --cookies-from-browser chrome
--- run_wiki_scrapper.sh
+++ run_wiki_scrapper.sh
@@ -1,13 +1,12 @@
-python wiki_scraper.py \
-  --wiki-api https://terraria.wiki.gg/api.php \
-  --game "Terraria" \
-  --biome-category cave_underground \
-# --page-title "Music" \
-  --dry-run
-
 # python wiki_scraper.py \
 #   --wiki-api https://terraria.wiki.gg/api.php \
 #   --game "Terraria" \
 #   --biome-category cave_underground \
-#   --page-title "Music" \
-#   --out tracks_terraria.csv
+#   --dry-run
+
+python wiki_scraper.py \
+  --wiki-api https://terraria.wiki.gg/api.php \
+  --game "Terraria" \
+  --biome-category cave_underground \
+  --page-title "Music" \
+  --out tracks_terraria.csv
