b56dbc7c6828011e3a9138360f0bdc9c02ba8bf1
Donggyu Lee <47566711+dg-lee23@users.noreply.github.com>
2026-08-21 12:17:34 +0000

    maple classisfication done

--- ost_biome_dataset/runner.sh
+++ ost_biome_dataset/runner.sh
@@ -3,7 +3,7 @@
 ./scripts/scrape_and_classify.sh https://subnautica.fandom.com/api.php "Subnautica"  # 스크랩+분류 한 번에
 
 #   -> classified/_review.csv 열어서 final_category 채우기
-./scripts/review.sh                                                # review 반영
+./scripts/review.sh --default-drop                                                # review 반영
 
 ./scripts/download.sh city_machine 
 ./scripts/download.sh cave_underground                             
