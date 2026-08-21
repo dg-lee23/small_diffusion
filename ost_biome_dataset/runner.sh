ec980190b32f707593127206a3490cd323b63cd0
Donggyu Lee <47566711+dg-lee23@users.noreply.github.com>
2026-08-21 10:14:46 +0000

    downloaded audio files, edited classified.csv

--- ost_biome_dataset/runner.sh
+++ ost_biome_dataset/runner.sh
@@ -1,8 +1,15 @@
-./scripts/setup.sh                                                # 1회, 의존성/ffmpeg/yt-dlp 확인
+
 
 ./scripts/scrape_and_classify.sh https://terraria.wiki.gg/api.php "Terraria"   # 스크랩+분류 한 번에
 
 #   -> classified/_review.csv 열어서 final_category 채우기
 ./scripts/review.sh                                                # review 반영
 
-./scripts/download.sh cave_underground                             # 카테고리별 오디오 다운로드
+./scripts/download.sh cave_underground                             
+./scripts/download.sh city_machine 
+./scripts/download.sh desert_rock
+./scripts/download.sh forest_jungle 
+./scripts/download.sh hell 
+./scripts/download.sh sea_underwater -- --cookies-file www.youtube.com_cookies.txt
+./scripts/download.sh snow_ice
+./scripts/download.sh space 
